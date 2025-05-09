7403429d3b8d5b77e769bb95089a8e37d0f39c35
Zahyr Seferina <zahyr.seferina@gmail.com>
2025-05-09 08:30:28 +0200

    chore: linter and readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Text-Crop
+
+Created for personal use to reduce file size when using LLMs. The program remove all white spaces form the file inculding new lines and prints it to your terminal. LLMs can still figure out the context form the blob generated and it saves on tokens and characther limits from some LLMs.
+
+## Requirements
+
+- Nothing special all made with the C++ standard library
+- Make
+- GCC
+
+## Installation
+
+1. Clone the project
+2. Make
+   ```sh
+   make 
+   ```
+3. Run the generated program
+   ```sh
+   ./output/main <filename>
+   ```
+
+## Usage
+Run the program as or put in your PATH environment for easy access.
+
+```sh
+./output/main <filename>
+```
--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,32 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
         return 1;
     }
 
     std::ifstream file(argv[1]);
-    if (!file) {
+    if (!file)
+    {
         std::cerr << "Failed to open file: " << argv[1] << std::endl;
         return 1;
     }
 
     constexpr int BUFFER_SIZE = 1024;
     std::unique_ptr<char[]> buffer(new char[BUFFER_SIZE]);
-    
 
-    while (file) {
+    while (file)
+    {
         file.read(buffer.get(), BUFFER_SIZE);
-        
+
         std::string result(buffer.get(), file.gcount());
 
         result.erase(std::remove_if(result.begin(), result.end(), ::isspace),
-        result.end());
+                     result.end());
 
-        std::cout << result <<std::endl;
+        std::cout << result << std::endl;
     }
 
     return 0;
